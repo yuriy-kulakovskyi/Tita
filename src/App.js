fe4a49e88bfe104db01278f21355f1dfb5c3a04c
Yuriy Kulakovskyi <84936189+yuriy-kulakovskyi@users.noreply.github.com>
2022-08-16 18:33:53 +0300

    "About" section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import About from './components/About/About';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Menu from './components/Menu/Menu';
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Menu />
       <Home />
+      <About />
     </div>
   );
 }
